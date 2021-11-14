0648184aa33c92bde7fca454bfee557cbba4be60
Diane Tiblin <tiblindm@gmail.com>
2021-11-14 17:46:15 -0500

    function is coming along

--- static/js/logic.js
+++ static/js/logic.js
@@ -29,12 +29,39 @@ d3.json(url).then(function(response) {
     
     // Once we get a response, send the data.features object to the createFeatures function.
     //createFeatures(response.features);
-    Features= response.features
+    //bbox":[-179.9383,-60.1048,-3.41,179.75016666667,69.9175,604.48]}
+    var Features= response.features;
      console.log(Features);
+    var minDepth = response.bbox[2]
+    var maxDepth = response.bbox[5]
+    
     Features.forEach(function (feature) {
         //L.geoJson(feature).addTo(map)
-        L.circle([feature.geometry.coordinates[1],feature.geometry.coordinates[0]]).bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`).addTo(map);
-    })
+        //createfunction to get marker style
+        
+        var size = "";
+        if(feature.properties.mag > 5) {
+          size = 100000
+        }
+        else if (feature.properties.mag > 4) {
+          size = 50000
+        };
+
+        var color = "";
+        if(feature.geometry.coordinates[2] <20) {
+          color = "#FF4040"}
+        else if(feature.geometry.coordinates[2] < 50) {
+          color = "#EE3B3B"}
+        else if(feature.geometry.coordinates[2] < 100) {
+          color = "#CD3333"}
+        else if(feature.geometry.coordinates[2] < 300) {
+          color = "#A52A2A"}
+        else if (feature.geometry.coordinates[2] > 300) {
+          color = "#8B2323"
+        };
+
+        L.circle([feature.geometry.coordinates[1],feature.geometry.coordinates[0]],{radius: size, color: color,fillColor: color, fillOpacity: .4}).bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)} <br> magnitude:  ${feature.properties.mag} </p>`).addTo(map);
+});
 });
 
 
